2c0a50cc3695e2a75a074da5ef47e110614a2911
netoks <gediminas@netoks.lt>
2024-02-20 19:23:41 +0200

    printing any array using generics

--- _20240220_Generics/src/task01/Main.java
+++ _20240220_Generics/src/task01/Main.java
@@ -1,24 +1,16 @@
 package task01;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayNumbers = new ArrayList<>();
-        arrayNumbers.add(5);
-        arrayNumbers.add(1);
-        arrayNumbers.add(6);
+        Integer[] arrayNumbers = {5, 1, 6};
 
-        ArrayList<String> arrayStrings = new ArrayList<>();
-        arrayStrings.add("Congratulations");
-        arrayStrings.add("Vilnius");
-        arrayStrings.add("with 700th Birthday");
+        String[] arrayStrings = {"Congratulations", "Vilnius", "with 700th Birthday"};
 
         printArrayElements(arrayNumbers);
         printArrayElements(arrayStrings);
     }
 
-    public static <E> void printArrayElements(ArrayList<E> array) {
+    public static <E> void printArrayElements(E[] array) {
         for (E e : array) {
             System.out.println(e);
         }
